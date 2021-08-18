381f246406c4f55ba2b69c2284d780c87d0d46dc
Ola Mańczak <manczakola@gmail.com>
2021-08-18 23:02:03 +0200

    add menu positions database and Menu interface

---
+++ src/app/navbar/menu.ts
@@ -0,0 +1,7 @@
+export interface Menu {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+  image: string;
+}
---
+++ src/assets/data/dataMenu.ts
@@ -0,0 +1,67 @@
+import { Menu } from '../../app/navbar/menu';
+
+export const dataMenu: Menu[] = [
+  {
+    id: 1,
+    name: 'Beefy Burgers',
+    description: 'Great way to make your business appear trust and relevant.',
+    price: '$5',
+    image: '1.webp',
+  },
+  {
+    id: 2,
+    name: 'Burger Boys',
+    description: 'Great way to make your business appear trust and relevant.',
+    price: '$5',
+    image: '2.webp',
+  },
+  {
+    id: 3,
+    name: 'Burger Bizz',
+    description: 'Great way to make your business appear trust and relevant.',
+    price: '$5',
+    image: '3.webp',
+  },
+  {
+    id: 4,
+    name: 'Crackles Burger',
+    description: 'Great way to make your business appear trust and relevant.',
+    price: '$5',
+    image: '4.webp',
+  },
+  {
+    id: 5,
+    name: 'Bull Burgers',
+    description: 'Great way to make your business appear trust and relevant.',
+    price: '$5',
+    image: '5.webp',
+  },
+  {
+    id: 6,
+    name: 'Rocket Burgers',
+    description: 'Great way to make your business appear trust and relevant.',
+    price: '$5',
+    image: '6.webp',
+  },
+  {
+    id: 7,
+    name: 'Delish Burger',
+    description: 'Great way to make your business appear trust and relevant.',
+    price: '$5',
+    image: '7.webp',
+  },
+  {
+    id: 8,
+    name: 'Crackles Burger',
+    description: 'Great way to make your business appear trust and relevant.',
+    price: '$5',
+    image: '8.webp',
+  },
+  {
+    id: 9,
+    name: 'Bull Burgers',
+    description: 'Great way to make your business appear trust and relevant.',
+    price: '$5',
+    image: '9.webp',
+  },
+];
